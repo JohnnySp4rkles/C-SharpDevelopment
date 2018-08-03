a0a7b24ffb287747b704feff570fcff3d5c80f7d
Johnny Diamond <johnathan_diamond@hotmail.com>
2018-08-04 00:59:21 +0100

    First Commit!!

---
+++ 001/Welcome.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp._001 {
+    class Welcome {
+        static void Main(String[] agrs)
+        {
+            Console.WriteLine("Welcome to the C# Station Tutorial!");
+            // keep screen from going away
+            Console.ReadLine();
+        }
+    }
+}
