a47bb4255c3b8de1fccf240842791f00dead7b34
Johnny Diamond <johnathan_diamond@hotmail.com>
2018-08-04 18:29:09 +0100

    .

--- Lesson002/BinaryOperators.cs
+++ Lesson002/BinaryOperators.cs
@@ -6,13 +6,13 @@ namespace CSharp.Lesson002 {
         static void Main(string[] args) {
             int x = 7, y = 5;
 
-            Console.WriteLine("x + y: {0}", x + y); // x+y: 12
-            Console.WriteLine("x - y: {0}", x - y); // x-y: 2
-            Console.WriteLine("x * y: {0}", x * y); // x*y: 35
-            Console.WriteLine("x / y: {0}", x / y); // x/y: 1
-            Console.WriteLine("x / y: {0}", (float)x / (float)y); // x/y: 1.4
-            Console.WriteLine("x % y: {0}", x % y); // x%y: 2
-            Console.WriteLine("(x % y) + x: {0}", (x % y) + x); // (x%y) + x: 9
+            Console.WriteLine("x + y: {0}", x + y);                 // x+y: 12
+            Console.WriteLine("x - y: {0}", x - y);                 // x-y: 2
+            Console.WriteLine("x * y: {0}", x * y);                 // x*y: 35
+            Console.WriteLine("x / y: {0}", x / y);                 // x/y: 1
+            Console.WriteLine("x / y: {0}", (float)x / (float)y);   // x/y: 1.4
+            Console.WriteLine("x % y: {0}", x % y);                 // x%y: 2
+            Console.WriteLine("(x % y) + x: {0}", (x % y) + x);     // (x%y) + x: 9
             Console.ReadLine();
         }
     }
