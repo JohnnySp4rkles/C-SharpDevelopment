7b95ca6161658b1fdf99a07d5ca0249d60ec6ab1
Johnny Diamond <johnathan_diamond@hotmail.com>
2018-08-04 18:06:42 +0100

    Update names

---
+++ Lesson002/UnaryOperators.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharp.Lesson002 {
+    class UnaryOperators {
+
+        static void Main(string[] args) {
+            int unary = 0;
+
+            Console.WriteLine("Unary: {0}", unary);
+            Console.WriteLine("preIncrement: {0}", ++unary);
+            Console.WriteLine("preDecrement: {0}", --unary);
+            Console.WriteLine("postDecrement: {0}", unary--);
+            Console.WriteLine("postIncrement: {0}", unary++);
+            Console.WriteLine("sByte: {0}", (sbyte)(~unary));
+
+            Console.ReadLine();
+        }
+    }
+}
