f72ac77be7ec59a690721ea581eed3d7a258edcd
Johnny Diamond <johnathan_diamond@hotmail.com>
2018-08-04 02:02:53 +0100

    data types

---
+++ 002/Booleans.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharp._002
+{
+    class Booleans
+    {
+        static void Main(string[] args)
+        {
+            bool varA = true;
+            bool varB = false;
+
+            Console.WriteLine("The variable varA == {0}", varA);
+            Console.WriteLine("The variable varB == {0}", varB);
+            Console.ReadLine();
+        }
+    }
+}
