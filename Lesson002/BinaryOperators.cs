c42c43bb896e98c2b575574b941909e771fe0183
Johnny Diamond <johnathan_diamond@hotmail.com>
2018-08-04 18:20:51 +0100

    added binary operator class

---
+++ Lesson002/BinaryOperators.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharp.Lesson002 {
+    class BinaryOperators {
+
+        static void Main(string[] args) {
+            int x = 7, y = 5;
+
+            Console.WriteLine("x + y: {0}", x + y); // x+y: 12
+            Console.WriteLine("x - y: {0}", x - y); // x-y: 2
+            Console.WriteLine("x * y: {0}", x * y); // x*y: 35
+            Console.WriteLine("x / y: {0}", x / y); // x/y: 1
+            Console.WriteLine("x / y: {0}", (float)x / (float)y); // x/y: 1.4
+            Console.WriteLine("x % y: {0}", x % y); // x%y: 2
+            Console.WriteLine("(x % y) + x: {0}", (x % y) + x); // (x%y) + x: 9
+            Console.ReadLine();
+        }
+    }
+}
