a8035369a7e75bc35fbc5e9970b5e1da81115a3b
Johnny Diamond <johnathan_diamond@hotmail.com>
2018-08-04 01:25:54 +0100

    Interactive Input

---
+++ 001/003InteractiveInput.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp._001
+{
+    class _003InteractiveInput
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What's your name: ");
+            
+            // STYLE A
+            Console.WriteLine("Hello {0}", Console.ReadLine());
+
+            Console.WriteLine("What's your second name: ");
+            // STYLE B
+            String sName = Console.ReadLine();
+            Console.Write("Hello {0}", sName);
+
+            Console.ReadLine();
+
+        }
+    }
+}
