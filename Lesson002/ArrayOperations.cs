27a29faa4bbb6573518574475f447d94a06442b4
Johnny Diamond <johnathan_diamond@hotmail.com>
2018-08-04 18:34:56 +0100

    new file

---
+++ Lesson002/ArrayOperations.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace CSharp.Lesson002 {
+    class ArrayOperations {
+        static void Main(string[] args) {
+            int[] numbers = { 1, 2, 3 };
+        }
+    }
+}
