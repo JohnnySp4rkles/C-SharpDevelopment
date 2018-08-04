fa8c7c559217cdb7591755cc78b4c3ba595d0fd9
Johnny Diamond <johnathan_diamond@hotmail.com>
2018-08-04 01:13:09 +0100

    Second part of 001 done

---
+++ 001/GettingInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp._001
+{
+    class GettingInput
+    {
+        // This program will use the @params from the CLI arguments!
+        static void Main(string[] args)
+        {
+            // From the Debug menu, i specified args[0] as Johnny & args[1] as Diamond!
+            Console.WriteLine("Hello, {0} {1}", args[0], args[1]);
+            Console.WriteLine("Welcome to the C# Station Tutorial!");
+            Console.ReadLine();
+        }
+    }
+}
