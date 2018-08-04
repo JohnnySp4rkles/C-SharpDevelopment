db27c04ccb450b0d1427a70cc1c9513eb42a108f
Johnny Diamond <johnathan_diamond@hotmail.com>
2018-08-04 05:15:16 +0100

    Extra commenting.

--- 001/002GettingInput.cs
+++ 001/002GettingInput.cs
@@ -7,7 +7,7 @@ namespace CSharp._001
         // This program will use the @params from the CLI arguments!
         static void Main(string[] args)
         {
-            // From the Debug menu, i specified args[0] as Johnny & args[1] as Diamond!
+            // From the Debug menu, i specified args[0] as Johnny & args[1] as Diamond for the arguments.
             Console.WriteLine("Hello, {0} {1}", args[0], args[1]);
             Console.WriteLine("Welcome to the C# Station Tutorial!");
             Console.ReadLine();
